336dc72f1098c5b0f568dab466fcbc38014ce8f3
Milton Dey Mishu <114005218+softengmilton@users.noreply.github.com>
2024-11-15 07:53:47 +0000

    mobile nav added

--- src/@core/config/navigation.js
+++ src/@core/config/navigation.js
@@ -1,15 +1,23 @@
- const leftNavigation = [
+import HomeIcon from '../../assets/images/icon/home-icon.png'
+import HeartIcon from '../../assets/images/icon/heart-icon.png'
+import PriceIcon from '../../assets/images/icon/price-icon.png'
+
+
+const leftNavigation = [
     {
         url: '/',
-        name:'Home'
+        name: 'Home',
+        icon:HomeIcon
     },
     {
         url: '/explore',
-        name:'Explore'
+        name: 'Explore',
+        icon:HeartIcon
     },
     {
         url: '/pricing',
-        name:'Pricing'
+        name: 'Pricing',
+        icon:PriceIcon
      },
 
 
--- src/@layouts/MainNavigation.vue
+++ src/@layouts/MainNavigation.vue
@@ -45,119 +45,6 @@
     </div>
     <!-- END -->
 
-    <!-- MENU POPUP -->
-    <div class="menu-pop menu-pop1">
-        <span class="menu-pop-clo"><i class="fa fa-times" aria-hidden="true"></i></span>
-        <div class="inn">
-            <img src="../assets/images/logo-b.png" alt="" loading="lazy" class="logo-brand-only">
-            <p><strong>Best Wedding Matrimony</strong> lacinia viverra lectus. Fusce imperdiet ullamcorper metus eu
-                fringilla.Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-            <ul class="menu-pop-info">
-                <li><a href="#!"><i class="fa fa-phone" aria-hidden="true"></i>+92 (8800) 68 - 8960</a></li>
-                <li><a href="#!"><i class="fa fa-whatsapp" aria-hidden="true"></i>+92 (8800) 68 - 8960</a></li>
-                <li><a href="#!"><i class="fa fa-envelope-o" aria-hidden="true"></i>help@company.com</a></li>
-                <li><a href="#!"><i class="fa fa-map-marker" aria-hidden="true"></i>3812 Lena Lane City Jackson
-                        Mississippi</a></li>
-            </ul>
-            <div class="menu-pop-help">
-                <h4>Support Team</h4>
-                <div class="user-pro">
-                    <img src="../assets/images/profiles/1.jpg" alt="" loading="lazy">
-                </div>
-                <div class="user-bio">
-                    <h5>Ashley emyy</h5>
-                    <span>Senior personal advisor</span>
-                    <a href="enquiry.html" class="btn btn-primary btn-sm">Ask your doubts</a>
-                </div>
-            </div>
-            <div class="menu-pop-soci">
-                <ul>
-                    <li><a href="#!"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                    <li><a href="#!"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                    <li><a href="#!"><i class="fa fa-whatsapp" aria-hidden="true"></i></a></li>
-                    <li><a href="#!"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-                    <li><a href="#!"><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>
-                    <li><a href="#!"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
-    <!-- END -->
-
-    <!-- CONTACT EXPERT -->
-    <div class="menu-pop menu-pop2">
-        <span class="menu-pop-clo"><i class="fa fa-times" aria-hidden="true"></i></span>
-        <div class="inn">
-            <div class="menu-pop-help">
-                <h4>Support Team</h4>
-                <div class="user-pro">
-                    <img src="../assets/images/profiles/1.jpg" alt="" loading="lazy">
-                </div>
-                <div class="user-bio">
-                    <h5>Ashley emyy</h5>
-                    <span>Senior personal advisor</span>
-                    <a href="enquiry.html" class="btn btn-primary btn-sm">Ask your doubts</a>
-                </div>
-            </div>
-            <div class="menu-pop-soci">
-                <ul>
-                    <li><a href="#!"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                    <li><a href="#!"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                    <li><a href="#!"><i class="fa fa-whatsapp" aria-hidden="true"></i></a></li>
-                    <li><a href="#!"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-                    <li><a href="#!"><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>
-                    <li><a href="#!"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-                </ul>
-            </div>
-
-            <div class="late-news">
-                <h4>Latest news</h4>
-                <ul>
-                    <li>
-                        <div class="rel-pro-img">
-                            <img src="../assets/images/couples/1.jpg" alt="" loading="lazy">
-                        </div>
-                        <div class="rel-pro-con">
-                            <h5>Long established fact that a reader distracted</h5>
-                            <span class="ic-date">12 Dec 2023</span>
-                        </div>
-                        <a href="blog-detail.html" class="fclick"></a>
-                    </li>
-                    <li>
-                        <div class="rel-pro-img">
-                            <img src="../assets/images/couples/3.jpg" alt="" loading="lazy">
-                        </div>
-                        <div class="rel-pro-con">
-                            <h5>Long established fact that a reader distracted</h5>
-                            <span class="ic-date">12 Dec 2023</span>
-                        </div>
-                        <a href="blog-detail.html" class="fclick"></a>
-                    </li>
-                    <li>
-                        <div class="rel-pro-img">
-                            <img src="../assets/images/couples/4.jpg" alt="" loading="lazy">
-                        </div>
-                        <div class="rel-pro-con">
-                            <h5>Long established fact that a reader distracted</h5>
-                            <span class="ic-date">12 Dec 2023</span>
-                        </div>
-                        <a href="blog-detail.html" class="fclick"></a>
-                    </li>
-                </ul>
-            </div>
-
-            <!-- HELP BOX -->
-            <div class="prof-rhs-help">
-                <div class="inn">
-                    <h3>Tell us your Needs</h3>
-                    <p>Tell us what kind of service you are looking for.</p>
-                    <a href="enquiry.html">Register for free</a>
-                </div>
-            </div>
-            <!-- END HELP BOX -->
-        </div>
-    </div>
-    <!-- END -->
 
     <!-- MAIN MENU -->
     <div class="hom-top">
@@ -203,9 +90,9 @@
                             <span class="mobile-exprt" data-mob="dashbord">
                                 <img src="../assets/images/icon/users.svg" alt="">
                             </span>
-                            <span class="mobile-menu" data-mob="mobile">
+                            <!-- <span class="mobile-menu" data-mob="mobile">
                                 <img src="../assets/images/icon/menu.svg" alt="">
-                            </span>
+                            </span> -->
                         </div>
                     </div>
                     <!--END MOBILE MENU-->
@@ -215,127 +102,115 @@
     </div>
     <!-- END -->
 
-    <!-- EXPLORE MENU POPUP -->
-    <div class="mob-me-all mobile_menu">
-        <div class="mob-me-clo"><img src="../assets/images/icon/close.svg" alt=""></div>
-        <div class="mv-bus">
-            <h4><i class="fa fa-globe" aria-hidden="true"></i> EXPLORE CATEGORY</h4>
-            <ul>
-                <li><a href="all-profiles.html">Browse profiles</a></li>
-                <li><a href="wedding.html">Wedding page</a></li>
-                <li><a href="services.html">All Services</a></li>
-                <li><a href="plans.html">Join Now</a></li>
-            </ul>
-            <h4><i class="fa fa-align-center" aria-hidden="true"></i> All Pages</h4>
-            <ul>
-                <li><a href="all-profiles.html">All profiles</a></li>
-                <li><a href="profile-details.html">Profile details</a></li>
-                <li><a href="wedding.html">Wedding</a></li>
-                <li><a href="wedding-video.html">Wedding video</a></li>
-                <li><a href="services.html">Our Services</a></li>
-                <li><a href="plans.html">Pricing plans</a></li>
-                <li><a href="login.html">Login</a></li>
-                <li><a href="sign-up.html">Sign-up</a></li>
-                <li><a href="photo-gallery.html">Photo gallery</a></li>
-                <li><a href="photo-gallery-1.html">Photo gallery 1</a></li>
-                <li><a href="contact.html">Contact</a></li>
-                <li><a href="about.html">About</a></li>
-                <li><a href="blog.html">Blog</a></li>
-                <li><a href="blog-detail.html">Blog detail</a></li>
-                <li><a href="enquiry.html">Ask your doubts</a></li>
-                <li><a href="make-reservation.html">Make Reservation</a></li>
-                <li><a href="faq.html">FAQ</a></li>
-                <li><a href="coming-soon.html" target="_blank">Coming soon</a></li>
-                <li><a href="404.html">404</a></li>
-            </ul>
-            <div class="menu-pop-help">
-                <h4>Support Team</h4>
-                <div class="user-pro">
-                    <!-- <img src="images/profiles/1.jpg" alt="" loading="lazy"> -->
-                </div>
-                <div class="user-bio">
-                    <h5>Ashley emyy</h5>
-                    <span>Senior personal advisor</span>
-                    <a href="enquiry.html" class="btn btn-primary btn-sm">Ask your doubts</a>
-                </div>
-            </div>
-            <div class="menu-pop-soci">
-                <ul>
-                    <li><a href="#!"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                    <li><a href="#!"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                    <li><a href="#!"><i class="fa fa-whatsapp" aria-hidden="true"></i></a></li>
-                    <li><a href="#!"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-                    <li><a href="#!"><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>
-                    <li><a href="#!"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-                </ul>
-            </div>
-            <div class="late-news">
-                <h4>Latest news</h4>
-                <ul>
-                    <li>
-                        <div class="rel-pro-img">
-                            <!-- <img src="images/couples/1.jpg" alt="" loading="lazy"> -->
-                        </div>
-                        <div class="rel-pro-con">
-                            <h5>Long established fact that a reader distracted</h5>
-                            <span class="ic-date">12 Dec 2023</span>
-                        </div>
-                        <a href="blog-detail.html" class="fclick"></a>
-                    </li>
-                    <li>
-                        <div class="rel-pro-img">
-                            <!-- <img src="images/couples/3.jpg" alt="" loading="lazy"> -->
-                        </div>
-                        <div class="rel-pro-con">
-                            <h5>Long established fact that a reader distracted</h5>
-                            <span class="ic-date">12 Dec 2023</span>
-                        </div>
-                        <a href="blog-detail.html" class="fclick"></a>
-                    </li>
-                    <li>
-                        <div class="rel-pro-img">
-                            <!-- <img src="images/couples/4.jpg" alt="" loading="lazy"> -->
-                        </div>
-                        <div class="rel-pro-con">
-                            <h5>Long established fact that a reader distracted</h5>
-                            <span class="ic-date">12 Dec 2023</span>
-                        </div>
-                        <a href="blog-detail.html" class="fclick"></a>
-                    </li>
-                </ul>
-            </div>
-            <div class="prof-rhs-help">
-                <div class="inn">
-                    <h3>Tell us your Needs</h3>
-                    <p>Tell us what kind of service you are looking for.</p>
-                    <a href="enquiry.html">Register for free</a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- END MOBILE MENU POPUP -->
 
-    <!-- MOBILE USER PROFILE MENU POPUP -->
-    <div class="mob-me-all dashbord_menu">
-        <!-- <div class="mob-me-clo"><img src="images/icon/close.svg" alt=""></div> -->
-        <div class="mv-bus">
-            <div class="head-pro">
-                <!-- <img src="images/profiles/1.jpg" alt="" loading="lazy"> -->
-                <b>user profile</b><br>
-                <h4>Ashley emyy</h4>
-            </div>
-            <ul>
-                <li><a href="login.html">Login</a></li>
-                <li><a href="sign-up.html">Sign-up</a></li>
-                <li><a href="plans.html">Pricing plans</a></li>
-                <li><a href="all-profiles.html">Browse profiles</a></li>
-            </ul>
-        </div>
-    </div>
-    <!-- END USER PROFILE MENU POPUP -->
+
+    <nav class="mobile-nav">
+        <ul class="nav-list">
+            <li class="nav-item" v-for="(item, index) in leftNavigation" :key="index">
+                <RouterLink class="nav-link" :to="item.url">
+                    <img :src="item.icon" />
+                    {{ item.name }}
+                </RouterLink>
+            </li>
+        </ul>
+    </nav>
 </template>
 
 <script setup>
 
-import leftNavigation from '../@core/config/navigation.js';  // Correct path
+import leftNavigation from '../@core/config/navigation.js';
+
+
 </script>
+<style scoped>
+.mobile-nav {
+    background-color: #1E2A78;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 10px 0;
+    border-radius: 10px 10px 0 0;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    z-index: 99;
+    display: none;
+}
+
+.nav-list {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+
+.nav-item {
+    flex-grow: 1;
+    text-align: center;
+}
+
+.nav-link {
+    color: #fff;
+    text-decoration: none;
+    font-size: 12px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.nav-link span {
+    margin-top: 5px;
+}
+
+.icon-discover::before {
+    content: '\\2764';
+    font-size: 20px;
+}
+
+.icon-message::before {
+    content: '\\2709';
+    font-size: 20px;
+}
+
+.icon-match::before {
+    content: '\\1F493';
+    font-size: 20px;
+}
+
+.icon-activities::before {
+    content: '\\1F4C8';
+    font-size: 20px;
+}
+
+.icon-search::before {
+    content: '\\1F50D';
+    font-size: 20px;
+}
+
+@media (max-width: 990px) {
+    .mobile-nav {
+        padding: 15px 0;
+        justify-content: space-evenly;
+        display: block;
+    }
+
+    .nav-link {
+        font-size: 14px;
+    }
+
+    .nav-link span {
+        margin-top: 8px;
+        font-size: 16px;
+    }
+
+    .icon-discover::before,
+    .icon-message::before,
+    .icon-match::before,
+    .icon-activities::before,
+    .icon-search::before {
+        font-size: 24px;
+    }
+}
+</style>
